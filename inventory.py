8f60d52652dba86be949a49d56c1682d50bae2b0
MangroveCuckoo <mueller.cara@gmail.com>
2014-08-14 21:04:22 -0700

    Made inventory class
    Also updated the commandline to reflect new inventory class.

---
+++ inventory.py
@@ -0,0 +1,21 @@
+'''Inventory'''
+
+import spaceship
+
+class Inventory:
+    intro: 'This is the inventory. You may store and retrieve items.'
+
+    def __init__(self):
+        self._inv = {}
+
+    def store(self, item):
+        '''Stores an item in the inventory'''
+        self._inv[item.name] = item
+
+    def retrieve(self, item_name):
+        ''' Returns an item from the inventory'''
+        return self._inv.get(item_name, None)
+
+    def destroy(self, item_name):
+        ''' Destroys an item from the inventory'''
+        del self._inv[item_name]

765cdabe61be70a9efb4ef9088434616815140f6
MangroveCuckoo <mueller.cara@gmail.com>
2014-08-15 23:23:50 -0700

    Moved example components over to component.py
    Also changed 'weight' to 'mass.' My English minor demanded it.

--- spaceship.py
+++ spaceship.py
@@ -138,85 +138,6 @@ class Bus:
         return 'Bus({.path})'.format(self)
 
 
-class BasicGun:
-    counter = 1
-
-    def __init__(self):
-        self.id = uuid.uuid4()
-        self.name = '{}-{:03}'.format(type(self).__name__, self.counter)
-        self.counter += 1
-        self.ammo_port = AmmoPort32mm()
-        self.power_port = PowerPort()
-        self.cooling_port = CoolingPort()
-        self.attachment = MediumAttachment(self)
-        self.weight = 32
-        self.baseline_power_draw = 1
-
-        self.bus = Bus(self.name)
-
-
-class BasicChassis:
-    def __init__(self):
-        self.power_port = PowerPort()
-        self.ammo_port = AmmoPort32mm()
-        self.cooling_port = CoolingPort()
-        self.attachments = {
-            'A': [MediumAttachment(self), MediumAttachment(self)],
-            'B': [SmallAttachment(self), SmallAttachment(self)],
-        }
-
-        self.weight = 50
-        self.baseline_power_draw = 0
-        self.baseline_cooling_draw = 0
-
-        self.bus = Bus()
-
-
-class CoolingConduit:
-    def __init__(self):
-        self.port_a = None
-        self.port_b = None
-        self.bandwidth = 34
-        self.bus = Bus()
-
-    def attach(self, port_a, port_b=None):
-        '''Attach the cable to one or more ports'''
-        self.port_a = port_a
-        self.port_b = port_b
-
-
-class AmmoFeed32mm:
-    def __init__(self):
-        self.out_port = None
-        self.in_port = None
-        self.bandwidth = 32
-        self.latency = 1
-
-    def attach(self, in_port=None, out_port=None):
-        self.in_port = in_port
-        self.out_port = out_port
-
-
-class PowerCable:
-    def __init__(self):
-        self.port_a = None
-        self.port_b = None
-        self.bandwidth = 50
-
-    def attach(self, port_a, port_b=None):
-        self.port_a = port_a
-        self.port_b = port_b
-
-
-class AmmoPort32mm:
-    pass
-
-class PowerPort:
-    pass
-
-class CoolingPort:
-    pass
-
 
 class Attachment:
     def __init__(self, owner):
@@ -238,6 +159,15 @@ class MediumAttachment(Attachment):
 
 class SmallAttachment(Attachment):
     weight_tolerance = 25
+
+class AmmoPort32mm:
+    pass
+
+class PowerPort:
+    pass
+
+class CoolingPort:
+    pass
 
 
 class Terminal:

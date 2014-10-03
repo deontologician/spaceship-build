200d9d30a39dc11250bfb7692a8fa4b17d9d734f
Josh Kuhn <deontologician@gmail.com>
2014-10-03 00:15:08 -0700

    Companies now have fields and markets

--- pcg.py
+++ pcg.py
@@ -1,15 +1,16 @@
-from random import choice, randint, sample, shuffle
+from random import choice, randint, sample, shuffle, gauss
+from collections import namedtuple
 
 COMPANY_STEMS = [
     "arch",
     "aur",
-    "bio",
     "ban",
+    "bio",
     "chron",
     "col",
     "com",
-    'dev',
     'deka',
+    'dev',
     "drov",
     "gen",
     "jov",
@@ -18,6 +19,7 @@ COMPANY_STEMS = [
     "mal",
     "meta",
     "mil",
+    "lun",
     "nano",
     "ner",
     "ono",
@@ -26,17 +28,19 @@ COMPANY_STEMS = [
     "pur",
     "secu",
     "sky",
+    "sol",
     "star",
     "sun",
     "tel",
     "ter",
-    "tok",
-    "tren",
-    "tyc",
     "tir",
+    "tok",
+    'ton',
+    "tren",
     "uni",
     "verdu",
     "weis",
+    "yut",
 ]
 
 COMPANY_TYPES_LONG = [
@@ -52,6 +56,8 @@ COMPANY_TYPES_LONG = [
     "Interplanetary",
     "Universal",
     "Ventures",
+    "Laboratories",
+    "Syndicate",
 ]
 
 COMPANY_TYPES_SHORT = [
@@ -77,7 +83,6 @@ PREFIXES = [
     'e',
     'i',
     'o',
-    'u',
 ]
 
 JOINERS = [
@@ -108,19 +113,31 @@ SUFFIXES = [
     'tech',
     'uko',
     'us',
-
+    'tron',
+    'land',
+    'ani',
+    'tor',
+    ' supply',
+    ' products',
 ]
 
-FIELDS = [
-    'propulsion',
-    'power',
-    'cooling',
-    'ammunition',
-    'weaponry',
-    'life-support',
-    'waste-removal',
-    'sensors',
-]
+FIELDS = {
+    'ammunition': 1,
+    'armor': 1,
+    'cabling': 1,
+    'cooling': 1,
+    'electronics': 1,
+    'ftl-cores': 2,
+    'life-support': 2,
+    'power': 2,
+    'propulsion': 2,
+    'sensors': 1,
+    'shield generators': 2,
+    'structural': 2,
+    'waste-disposal': 1,
+    'weaponry': 1,
+    'wiring': 1,
+}
 
 def company_name(stems, suffixes, long_types):
     '''Creates a random company name. Deletes items from stems and
@@ -128,7 +145,6 @@ def company_name(stems, suffixes, long_types):
     run out!
     '''
     name = ''
-    has_suffix = False
     used_stems = []
     if not randint(0, 3):
         if not randint(0, 3):
@@ -139,10 +155,13 @@ def company_name(stems, suffixes, long_types):
     name += used_stems[-1]
     if not randint(0, 2):
         if randint(0, 1) and name[-1] not in 'aeiouy':
+            # prevent the hawaiian problem
             name += choice(JOINERS)
         used_stems.append(stems.pop())
         name += used_stems[-1]
     if not randint(0, 2) or name in used_stems:
+        if name.endswith('v'):
+            name += choice(JOINERS + ['e'])
         name += suffixes.pop()
     name = name.title()
     if randint(0, 1) and len(name) <= 7:
@@ -151,16 +170,53 @@ def company_name(stems, suffixes, long_types):
         name += ' ' + choice(COMPANY_TYPES_SHORT)
     return name
 
+def buy_fields(name, field_markets):
+    total_points = max(round(gauss(6, 3)), 1)
+    points = total_points
+    fields = {}
+    while points > 0 and field_markets:
+        (field, market), cost = choice(list(field_markets.items()))
+        fields[field] = market
+        del field_markets[(field, market)]
+        points -= cost
+    return fields
+
+
+class Company:
+    def __init__(self, name, fields):
+        self.name = name
+        self.fields = fields
+
+
 def make_companies():
-    '''Creates 15 companies'''
+    '''Creates several companies'''
     stems = COMPANY_STEMS[:]
     suffixes = SUFFIXES[:]
     long_types = COMPANY_TYPES_LONG[:]
     shuffle(stems)
     shuffle(suffixes)
     shuffle(long_types)
-    return [company_name(stems, suffixes, long_types) for _ in range(15)]
+    markets = {
+        'high-end': 3,
+        'mid-market': 2,
+        'low-end': 1,
+    }
+    field_markets = {(field, market): fp * mp
+                     for field, fp in FIELDS.items()
+                     for market, mp in markets.items()}
+    companies = []
+    while field_markets:
+        name = company_name(stems, suffixes, long_types)
+        fields = buy_fields(name, field_markets)
+        companies.append(Company(name, fields))
+    return companies
+
 
 if __name__ == '__main__':
-    for company in make_companies():
-        print(company)
+    companies = make_companies()
+    for company in companies:
+        print(company.name)
+        for field, market in company.fields.items():
+            print('  ', market, field)
+    print(len(companies), 'created')
+    

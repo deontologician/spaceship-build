2c9b84ec61bf14a2dbd24cad6f695050389ae35e
Josh Kuhn <deontologician@gmail.com>
2014-10-21 00:43:53 -0700

    Preparing for product line generation

--- pcg.py
+++ pcg.py
@@ -1,143 +1,64 @@
 from random import choice, randint, sample, shuffle, gauss, random
-import re
-from collections import namedtuple
+from itertools import product
+from copy import deepcopy
+import string
+from pcg_data import (COMPANY_STEMS, PRE_TERMINALS, TERMINALS,
+                      RARE_PREFIXES, JOINERS, SUFFIXES, WORD_GROUPS,
+                      GREEK_LETTERS)
 
-COMPANY_STEMS = [
-    "arch",
-    ('atom', ['', 'i', 'ic']),
-    ('agis', ['']),
-    "aur",
-    ("ban", ['a', 'i', 'u',]),
-    "bio",
-    ("chron", ['a', 'i', '']),
-    "col",
-    "com",
-    'deka',
-    "gen",
-    ("jov", ['a','i','ia']),
-    ("kang", ['u','a']),
-    ("mal", ['a', 'e', 'i']),
-    ("met", ['a']),
-    ("mil", ['a', 'io', 'i', 'li']),
-    ("lun", ['a', 'ar', 'i']),
-    ("nan", ['o', 'i']),
-    ("ner", ['']),
-    ("on", ['o', 'a', 'i', 'u']),
-    ("plex", ['i', 'a', '', 'o']),
-    "prot",
-    ("pur", ['a', 'i', 'e']),
-    ("sec", ['u', 'ure']),
-    ("sk", ['y']),
-    ("sol", ['a','i']),
-    "star",
-    ("sun", ['']),
-    ("tel", ['e', 'a']),
-    ("ter", ['a', 'ra', 'ra ', 'i', 'u', 'o']),
-    "tir",
-    ("tok", ['a', 'u', 'i']),
-    ('ton', ['i', 'a', 'u', 'o']),
-    "tren",
-    ("un", ['i', 'a', 'e']),
-    ("verd", ['u', 'a', 'ant']),
-    ("w", ['eis', 'yl', 'y', 'ey', 'eyl']),
-    "yut",
-]
+# Each field has a point cost. Companies that specialize in higher
+# point-cost fields will have less fields overall (usually)
 
-PRE_TERMINALS = [
-    "Design",
-    "Dynamics",
-    "Engineering",
-    "Enterprises",
-    "Foundation",
-    "Galactic",
-    "Incorporated",
-    "Industries",
-    "International",
-    "Interplanetary",
-    "Laboratories",
-    "Limited",
-    "Manufacturing",
-    "Orbital",
-    "Products",
-    "Supply",
-    "Syndicate",
-    "Universal",
-    "Ventures",
-]
+# Naming schemes
+# Something ending in "Pro"
+# - Chiron, Chiron Pro
+# - '{} {}Pro'.format(noun(), adjective())
+# Something with 2-3 letters ending with X
+# - ATX, DX, MX, LX
+# - upper_letters(randint(1, 2)) + 'X'
+# number schemes
+# - 2511, 8511, 9511 # ending numbers the same
+# - '{{}}{}'.format(digits(3)).format(model_level + 1)
+# - 9512, 9524, 9550 # starting numbers the same
+# - 
+# - 50, 60, 70 # Just some sort of sequential numbers
+# Letter and number
+# - M23, F13, X-23
+# - Always using the same letter, or letter per line
+# Names based on provocative stuff
+# - dragoon, python, peacemaker, cobra, defender
+# Letters hyphen numbers letter
+# - FRD-1020R
+# Letters, hyphen then single number
+# - GE10-1, GE10-2
+# Series Roman numeral
+# - Series II, Series III
+# Mark Roman numeral
+# - Mark I, Mark II
+# Compound neologisms
+# - Nightreaver
+# - SandTamer
+# And of course: Names that have something to do with the product and its attributes
+# - 200mm long-range rifle
+# - 400MW Generator
 
-TERMINALS = [
-    "Co.",
-    "Llc.",
-    "Ltd.",
-    "Inc.",
-    "Corp.",
-    "Company",
-    "Corporation",
-]
 
-RARE_PREFIXES = [
-    'epi',
-    'meta',
-    'hyper',
-    'itsu',
-    'astral',
-    'sub',
-]
+def digits(n):
+    '''Yields an n digit number'''
+    return ('{}'*n).format(randint(1, 9), *(randint(0,9) for _ in range(n-1)))
 
-JOINERS = [
-    'a',
-    'e',
-    'i',
-    'o',
-    'u',
-    ' ',
-    '',
-]
 
-SUFFIXES = [
-    'al',
-    'dyn',
-    'ex',
-    'dev',
-    'gen',
-    'ogia',
-    'hama',
-    'hiko',
-    'hito',
-    'ia',
-    'ic',
-    'ine',
-    'is',
-    'logic',
-    'man',
-    'ology',
-    'sec',
-    'tech',
-    'uko',
-    'us',
-    'tron',
-    'land',
-    'ani',
-    'tor',
-]
+def upper_letters(n):
+    '''Yields a random string of uppercase letters n characters long'''
+    return ''.join(sample(string.ascii_uppercase, n))
 
-FIELDS = {
-    'ammunition': 1,
-    'armor': 1,
-    'cabling': 1,
-    'cooling': 1,
-    'electronics': 1,
-    'ftl-cores': 2,
-    'life-support': 2,
-    'power': 2,
-    'propulsion': 2,
-    'sensors': 1,
-    'shield generators': 2,
-    'structural': 2,
-    'waste-disposal': 1,
-    'weaponry': 1,
-    'wiring': 1,
-}
+
+# Heirarchy of objects:
+# Company -> Field -> Market -> ProductLine -> Model
+# each relationship is one to many
+
+
+# TODO: define durability in terms of hours of expected usage
 
 class Name(str):
     '''A string subclass that keeps track of its constituent parts'''
@@ -172,14 +93,8 @@ def extract(stem):
         stem = stem
     return stem, joiners
 
-def corp_name(stems=None, suffixes=None):
+def corp_name(stems, suffixes):
     '''Creates the non-sensical part of the name'''
-    if stems is None:
-        stems = COMPANY_STEMS[:]
-        shuffle(stems)
-    if suffixes is None:
-        suffixes = SUFFIXES[:]
-        shuffle(suffixes)
     name = Name()
     first_stem = None
     second_stem = None
@@ -207,7 +122,7 @@ def normalize(name):
         .replace('ea', 'e a')\
         .replace('oex', 'ex')\
         .replace('ii', 'i')\
-        .replace('oo', 'o')\
+        .replace('oo', 'oö')\
         .replace('aa', 'a')\
         .replace('uu', 'u')\
         .replace('hh', 'h')\
@@ -217,85 +132,116 @@ def normalize(name):
     ret.pieces = name.pieces[:]
     return ret
 
-def company_name(stems=None, suffixes=None, pre_terminals=None):
-    '''Creates a random company name. Deletes items from stems and
-    suffixes, so don't pass in the global versions or you'll quickly
-    run out!
-    '''
-    if pre_terminals is None:
-        pre_terminals = PRE_TERMINALS[:]
-        shuffle(pre_terminals)
-    name = corp_name(stems, suffixes)
-    name = name.title()
-    if with_probability(1/2):
-        name += ' ' + choice(pre_terminals)
-    if with_probability(2/3):
-        name += ' ' + choice(TERMINALS)
-    return name
 
-def buy_fields(name, field_markets):
-    total_points = max(round(gauss(6, 4)), 1)
-    points = total_points
-    fields = {}
-    while points > 0 and field_markets:
-        (field, market), cost = choice(list(field_markets.items()))
-        fields[field] = market
-        del field_markets[(field, market)]
-        points -= cost
-    return fields
+class NameParts:
+    def __init__(self):
+        self.stems = COMPANY_STEMS[:]
+        shuffle(self.stems)
+        self.suffixes = SUFFIXES[:]
+        shuffle(self.suffixes)
+        self.pre_terminals = PRE_TERMINALS[:]
+        shuffle(self.pre_terminals)
 
 
 class Company:
-    def __init__(self, name, fields):
-        self.name = name
-        self.fields = fields
+    '''Represents a producer of goods'''
+    def __init__(self, parts, field_markets):
+        self.initial_points = max(round(gauss(6, 4)), 1)
+        self.name = self._create_name(parts)
+        self.fields, self.point_debt = self._buy_fields(field_markets)
 
+    @property
+    def points(self):
+        return self.initial_points - self.point_debt
 
-def make_companies():
-    '''Creates several companies'''
-    stems = COMPANY_STEMS[:]
-    suffixes = SUFFIXES[:]
-    pre_terminals = PRE_TERMINALS[:]
-    shuffle(stems)
-    shuffle(suffixes)
-    shuffle(pre_terminals)
-    markets = {
+    def _create_name(self, parts):
+        '''Creates a random company name. Deletes items from stems and
+        suffixes, so don't pass in the global versions or you'll quickly
+        run out!
+        '''
+        name = corp_name(parts.stems, parts.suffixes)
+        name = name.title()
+        if with_probability(1/2):
+            name += ' ' + choice(parts.pre_terminals)
+        if with_probability(2/3):
+            name += ' ' + choice(TERMINALS)
+        return name
+
+    def _buy_fields(self, field_markets):
+        '''Purchases several fields for the company based on the
+        number of points it received'''
+        points = self.initial_points
+        fields = []
+        while points > 0 and field_markets:
+            index = randint(0, len(field_markets) - 1)
+            fieldmarket = field_markets[index]
+            fields.append(fieldmarket)
+            del field_markets[index]
+            points -= fieldmarket.cost
+        return fields, points
+
+    def create_lines(self, word_groups):
+        '''Create all product lines, given the provided word_groups'''
+        
+
+class FieldMarket:
+    FIELDS = {
+        'ammunition': 1,
+        'armor': 1,
+        'cabling': 1,
+        'cooling': 1,
+        'electronics': 1,
+        'ftl-cores': 2,
+        'life-support': 2,
+        'power': 2,
+        'propulsion': 2,
+        'sensors': 1,
+        'shield generators': 2,
+        'structural': 2,
+        'waste-disposal': 1,
+        'weaponry': 1,
+        'wiring': 1,
+    }
+    
+    MARKETS = {
         'high-end': 4,
         'mid-market': 2,
         'low-end': 1,
     }
-    field_markets = {(field, market): fp * mp
-                     for field, fp in FIELDS.items()
-                     for market, mp in markets.items()}
-    companies = []
-    while field_markets:
-        name = company_name(stems, suffixes, pre_terminals)
-        fields = buy_fields(name, field_markets)
-        companies.append(Company(name, fields))
+
+    def __init__(self, field, market):
+        self.field = field
+        self.market = market
+        self.cost = self.FIELDS[field] * self.MARKETS[market]
+
+    def __str__(self):
+        return '{} {}'.format(self.level, self.field)
+
+    @staticmethod
+    def all_combinations():
+        '''Create a list of all combinations of fields and markets'''
+        return [FieldMarket(field, market)
+                for field, market in 
+                product(FieldMarket.FIELDS, FieldMarket.MARKETS)]
+
+def make_companies():
+    '''Creates companies'''
+                     
+    parts = NameParts()
+    field_markets = FieldMarket.all_combinations()
+    companies = [Company(parts, field_markets)
+                 for _ in range(100) if field_markets]
+    word_groups = deepcopy(WORD_GROUPS)
+    companies.sort(key=lambda c: c.points, reverse=True)
+    for company in companies:
+        company.create_lines(word_groups)
     return companies
 
-def build_trie():
-    assoc = {}
-    while True:
-        try:
-            suggested = corp_name()
-            answer = input(suggested.title() + ' [y/N]: ')
-            if answer in ('Y','y','yes'):
-                if len(suggested.pieces) > 1:
-                    for fst, snd in zip(suggested.pieces[:-1], suggested.pieces[1:]):
-                        assoc[fst] = snd
-                else:
-                    assoc[fst] = ''
-
-        except KeyboardInterrupt:
-            break
-    return assoc
 
 if __name__ == '__main__':
     companies = make_companies()
     for company in companies:
-        print(company.name)
-        print('    ', company.name.pieces)
-        #for field, market in company.fields.items():
-        #    print('  ', market, field)
+        print(company.name, ':', company.points)
+        for fieldmarket in company.fields:
+            print('  ', fieldmarket.market, fieldmarket.field, ':', fieldmarket.cost)
     print(len(companies), 'created')
